d73b5114b1fe92e2a1d37b8f655242e5017ba1b2
yorryoy1e <yorrygalvina@gmail.com>
2025-05-21 22:33:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.titl ("Selamat Datang di Web Informatika")
 st.write(
     "ngodingseru bersama Bapak Hendri Setiadi"
 )
+st.image("https://github.com/yorryoy1e/yorrygalvina/blob/main/42B3FA95-27B4-4596-A110-6A92A91B807E.png", width=200)
